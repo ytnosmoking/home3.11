0b8c942f97b68e27ba546e56dc7904c13630210a
ytnosmoking <349915792@qq.com>
2018-03-11 20:52:02 +0800

    git test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div>test git</div>
+    <div>test git1</div>
     <router-link to="/index"> index</router-link>
     <router-link to="/dashboard">dashboard</router-link>
     <transition mode="out-in"
